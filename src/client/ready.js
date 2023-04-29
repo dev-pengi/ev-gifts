740c9c4b7981a1689c76b21dd6a787627427edf2
dev-pengi <siffffdoul@gmail.com>
2023-04-29 05:43:51 +0100

    full update

--- src/client/ready.js
+++
@@ -1,7 +0,0 @@
-const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
-module.exports = async (client) => {
-    console.log(
-        `\nconnected to: ${client.user.tag}`,
-    )
-    client.user.setActivity('discord.gg/ev', { type: 'WATCHING' });
-}
